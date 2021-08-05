2f1cac37ccbf041408710ea4fd149ea4cd2fde44
AmirHosseinJahangiri <amirhosseinjahangiri78@gmail.com>
2021-08-05 11:42:56 +0430

    update product detail screen and separate logic and ui application

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,17 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../providers/products.dart';
 
 class ProductDetailScreen extends StatelessWidget {
   static const String routeName = '/product-detail';
 
-  final String? title;
-  const ProductDetailScreen({this.title});
+  final String? id;
+  const ProductDetailScreen({this.id});
 
   @override
   Widget build(BuildContext context) {
-    final productId = ModalRoute.of(context)!.settings.name as String;
+    final productId = ModalRoute.of(context)!.settings.arguments as String;
+    final loadedProduct = Provider.of<Products>(context , listen: true).findById(productId);
     return Scaffold(
-      appBar: AppBar(title: Text('title')),
+      appBar: AppBar(title: Text(loadedProduct.title!)),
+      body: Center(
+        child: Text(loadedProduct.description!, style: TextStyle(fontWeight: FontWeight.bold , fontSize: 20),),
+      ),
     );
   }
 }
